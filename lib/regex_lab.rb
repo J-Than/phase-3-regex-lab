cd5c1e7299b912491335e8390ae24e2cd52bccf3
Jarod Wilson <jaroddwilson@gmail.com>
2022-09-04 14:59:40 -0400

    commit tree 47f96f91765d71bf1521c65e13b9ff955970b741

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  true & word.match(/^[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  true & text.match(/^[A-Z].*[!?.;:,]\z/)
 end
 
 def valid_phone_number?(phone)
-
+  true & phone.match(/^\D?\d{3}\D?\d{3}\D?\d{4}$/)
 end
